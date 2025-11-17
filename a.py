683b81796885f4ddea74e4fe717dc5bdb9d6c81d
mcsexam <rohanjadhavyt07@gmail.com>
2025-11-17 10:21:41 +0530

    Refactor minimax function and update main execution

--- a.py
+++ a.py
@@ -1,24 +1,25 @@
 import math
 
-def minimax(depth, nodeIndex, isMax, scores, h):
-    if depth == h:
+def minimax(curDepth, nodeIndex, maxTurn, scores, targetDepth):
+    if curDepth == targetDepth:
         return scores[nodeIndex]
 
-    if isMax:
+    if maxTurn:
         return max(
-            minimax(depth + 1, nodeIndex * 2, False, scores, h),  
-            minimax(depth + 1, nodeIndex * 2 + 1, False, scores, h)  
+            minimax(curDepth + 1, nodeIndex * 2, False, scores, targetDepth),
+            minimax(curDepth + 1, nodeIndex * 2 + 1, False, scores, targetDepth),
         )
     else:
         return min(
-            minimax(depth + 1, nodeIndex * 2, True, scores, h), 
-            minimax(depth + 1, nodeIndex * 2 + 1, True, scores, h) 
+            minimax(curDepth + 1, nodeIndex * 2, True, scores, targetDepth),
+            minimax(curDepth + 1, nodeIndex * 2 + 1, True, scores, targetDepth),
         )
 
-scores = [3, 5, 2, 9]
 
-h = int(math.log(len(scores), 2))
+if __name__ == "__main__":
+    scores = [3, 5, 2, 9, 12, 5, 23, 23]
+    treeDepth = int(math.log2(len(scores)))  # convert to integer depth
 
-res = minimax(0, 0, True, scores, h)
-
-print("The optimal value is:", res)
+    print("The optimal value is : ", end="")
+    print(minimax(0, 0, True, scores, treeDepth))
+    
